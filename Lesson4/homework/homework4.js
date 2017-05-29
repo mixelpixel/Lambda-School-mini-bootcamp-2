96d20a5adaec5bd489c6635be7b5e0a6d21f7138
mixelpixel <mixelpix@mac.com>
2017-05-29 15:47:03 -0400

    js extra credit img

--- Lesson4/homework/homework4.js
+++ Lesson4/homework/homework4.js
@@ -10,7 +10,7 @@ document.body.style.fontFamily = 'sans-serif';
 // 4. Add your nickname
 document.getElementById('nickname').innerHTML = 'JavaScript says it\'s, "mixelpix."';
 // 5. Add your favorites
-document.getElementById('favorites').innerHTML = 'JavaScript says it\'s, "Korean food, my dog \'Lucy\' analytical philosophy."';
+document.getElementById('favorites').innerHTML = 'JavaScript says it\'s, "Korean food, my dog \'Lucy\' & philosophy."';
 // 6. Add your hometown
 document.getElementById('hometown').innerHTML = 'JavaScript says it\'s, "Kailua, HI."';
 
@@ -28,11 +28,16 @@ var allListItemsByClass = document.getElementsByClassName('listitem');
 // https://www.w3schools.com/css/css_text.asp
 for (var i = 0; i < allListItemsByClass.length; i++) {
   allListItemsByClass[i].style.color = '#ff0000';
-  allListItemsByClass[i].style.backgroundColor = '#00ff00';
+  allListItemsByClass[i].style.backgroundColor = '#ffff00';
 };
 
 // Extra Credit 2: Create a new img element and set its src attribute to a picture of you.
-
-
+var image = document.createElement('img');
+image.setAttribute('src', 'IMG_1488.JPG');
+image.setAttribute('alt', 'JavaScript says, "it\'s Lucy wearing mixelpix\'s gardening hat!"');
+image.style.transform = 'rotate(90deg)';
+image.style.paddingTop = '150px';
+image.style.width='100%';
 
 // Extra Credit 2: Append that element to the page.
+document.body.appendChild(image);
