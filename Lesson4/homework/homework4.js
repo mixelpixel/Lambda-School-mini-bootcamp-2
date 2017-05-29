abcdef51d8754a91853420d414e6ab6f0fa7d1bf
mixelpixel <mixelpix@mac.com>
2017-05-29 12:55:04 -0400

    js text content

--- Lesson4/homework/homework4.js
+++ Lesson4/homework/homework4.js
@@ -1,7 +1,9 @@
 document.getElementById('about_header').innerHTML = 'JavaScript\'s About Me';
 document.getElementById('about_header').style.backgroundColor = '#0000ff';
 
-// document.getElementById('nickname').innerHTML = 'JavaScript says it\'s, "Paprika"';
+document.getElementById('nickname').innerHTML = 'JavaScript says it\'s, "mixelpix."';
+document.getElementById('favorites').innerHTML = 'JavaScript says it\'s, "Korean food, my dog \'Lucy\' analytical philosophy."';
+document.getElementById('hometown').innerHTML = 'JavaScript says it\'s, "Kailua, HI."';
 
 document.body.style.backgroundColor = '#ff0000';
 document.body.style.fontFamily = 'sans-serif';
