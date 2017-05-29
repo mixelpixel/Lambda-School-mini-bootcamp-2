a4c0357beecfe6411ecbcc00632d46d48f50536c
mixelpixel <mixelpix@mac.com>
2017-05-29 11:12:54 -0400

    Homework 4

--- Lesson4/homework/homework4.js
+++ Lesson4/homework/homework4.js
@@ -2,6 +2,6 @@ document.getElementById('about_header').innerHTML = 'JavaScript\'s About Me';
 
 document.getElementById('nickname').innerHTML = 'JavaScript says it\'s, "Paprika"';
 
-var el = document.createElement('div');
-elem.innerHTML = '<p>Hello World!</p>';
-document.body.appendChild(el);
+// var el = document.createElement('div');
+// var elem.innerHTML = '<p>Hello World!</p>';
+// document.body.appendChild(el);
