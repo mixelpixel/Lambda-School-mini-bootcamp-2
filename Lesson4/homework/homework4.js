0d0a2e072bcbfe0c4abd0b0b07d1b9c0be3d49d9
mixelpixel <mixelpix@mac.com>
2017-05-29 12:49:25 -0400

    css nickname text content

--- Lesson4/homework/homework4.js
+++ Lesson4/homework/homework4.js
@@ -1,7 +1,7 @@
 document.getElementById('about_header').innerHTML = 'JavaScript\'s About Me';
 document.getElementById('about_header').style.backgroundColor = '#0000ff';
 
-document.getElementById('nickname').innerHTML = 'JavaScript says it\'s, "Paprika"';
+// document.getElementById('nickname').innerHTML = 'JavaScript says it\'s, "Paprika"';
 
 document.body.style.backgroundColor = '#ff0000';
 document.body.style.fontFamily = 'sans-serif';
