6539292f79d2d95f37a4fbbf68ebb8b2a2d42378
mixelpixel <mixelpix@mac.com>
2017-05-29 12:20:21 -0400

    js body sans-serif

--- Lesson4/homework/homework4.js
+++ Lesson4/homework/homework4.js
@@ -4,3 +4,4 @@ document.getElementById('about_header').style.backgroundColor = '#0000ff';
 document.getElementById('nickname').innerHTML = 'JavaScript says it\'s, "Paprika"';
 
 document.body.style.backgroundColor = '#ff0000';
+document.body.style.fontFamily = 'sans-serif';
