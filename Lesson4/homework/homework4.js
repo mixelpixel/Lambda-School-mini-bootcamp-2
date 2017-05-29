8d889933ee97ec7bbd08d03533b1e7f9031fbb31
mixelpixel <mixelpix@mac.com>
2017-05-29 11:29:15 -0400

    document.body.style.backgroundColor = 'red';

--- Lesson4/homework/homework4.js
+++ Lesson4/homework/homework4.js
@@ -2,6 +2,4 @@ document.getElementById('about_header').innerHTML = 'JavaScript\'s About Me';
 
 document.getElementById('nickname').innerHTML = 'JavaScript says it\'s, "Paprika"';
 
-// var el = document.createElement('div');
-// var elem.innerHTML = '<p>Hello World!</p>';
-// document.body.appendChild(el);
+document.body.style.backgroundColor = "red";
