a467646ad231045e7efcf2c793180a6f4d2253e8
mixelpixel <mixelpix@mac.com>
2017-05-29 11:32:10 -0400

    replace double quotes with single quotes;

--- Lesson4/homework/homework4.js
+++ Lesson4/homework/homework4.js
@@ -2,4 +2,4 @@ document.getElementById('about_header').innerHTML = 'JavaScript\'s About Me';
 
 document.getElementById('nickname').innerHTML = 'JavaScript says it\'s, "Paprika"';
 
-document.body.style.backgroundColor = "red";
+document.body.style.backgroundColor = 'red';
