6dd6478ac7805c62425d44af83f9e1ea3ba48577
mixelpixel <mixelpix@mac.com>
2017-06-07 07:44:14 -0400

    roundUp

--- Lesson6/lswebhomework6-master/exercises.js
+++ Lesson6/lswebhomework6-master/exercises.js
@@ -118,7 +118,7 @@ function roundNumber(num) {
 
 function roundUp(num) {
   //round num up and return it
-  return Math.roundUp(num);
+  return Math.ceil(num);
 }
 
 function addExclamationPoint(str) {
