b6c316cfd9e41cd46ea86b8642b444af56ffcb21
mixelpixel <mixelpix@mac.com>
2017-06-07 07:51:34 -0400

    getCircleArea

--- Lesson6/lswebhomework6-master/exercises.js
+++ Lesson6/lswebhomework6-master/exercises.js
@@ -153,7 +153,7 @@ function getTriangleArea(base, height) {
 
 function getCircleArea(radius) {
   //return the rounded area of the circle given the radius
-  return Math.PI * Math.pow(radius, 2);
+  return Math.round(Math.PI * Math.pow(radius, 2));
 }
 
 function getRectangularPrismVolume(length, width, height) {
